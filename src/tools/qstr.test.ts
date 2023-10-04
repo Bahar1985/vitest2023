03edb5c54a201594f7a232800a76f1abb92e5459
Sedigheh Balyavi <bayavisedigheh@gmail.com>
2023-10-05 01:22:38 +0200

    added testing

---
+++ src/tools/qstr.test.ts
@@ -0,0 +1,10 @@
+import {capitalizeFirstLetter} from "./qstr";
+
+describe('capitalizeFirstLetter()', () =>{
+
+    it('uppercases correct letters', () => {
+
+        expect(capitalizeFirstLetter('testing').toBe('Testing'))
+    });
+
+});
